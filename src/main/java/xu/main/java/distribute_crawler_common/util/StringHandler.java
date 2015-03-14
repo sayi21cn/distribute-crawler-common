bb4fc531555776c2d62d76b0e261f0c0a3004013
xu <da7xiongmao@gmail.com>
2015-03-14 15:46:57 +0800

    add function:string2Int

--- src/main/java/xu/main/java/distribute_crawler_common/util/StringHandler.java
+++ src/main/java/xu/main/java/distribute_crawler_common/util/StringHandler.java
@@ -2,6 +2,15 @@ package xu.main.java.distribute_crawler_common.util;
 
 public class StringHandler {
 
+	public static int string2Int(String input, int defaultNum) {
+		int result = defaultNum;
+		try {
+			result = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+		}
+		return result;
+	}
+
 	public static boolean isNullOrEmpty(String input) {
 		return null == input || "".equals(input);
 	}
